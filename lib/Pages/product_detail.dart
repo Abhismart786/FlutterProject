1c5f3b2cf7d08d7ca3193ff1452a885c6021ca57
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-13 11:01:46 -0500

    15 commit

--- lib/Pages/product_detail.dart
+++ lib/Pages/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class ProductDetail extends StatefulWidget {
   const ProductDetail({super.key});
 
@@ -11,10 +12,44 @@ class _ProductDetailState extends State<ProductDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFfef5f1),
-      body: Container(child: Column(children: [
-
-        Image.asset("images/headphone2.png")
-      ],),),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0, left: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Icon(Icons.arrow_back_ios_new_outlined),
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                "images/headphone2.png",
+                height: 300,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: Text("data"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
