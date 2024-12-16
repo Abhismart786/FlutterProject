100fcc3c309034290300557a9290b20f72ce7353
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-16 10:50:28 -0500

    17 commit

--- lib/Pages/product_detail.dart
+++ lib/Pages/product_detail.dart
@@ -22,7 +22,7 @@ class _ProductDetailState extends State<ProductDetail> {
                 Navigator.pop(context);
               },
               child: Container(
-                margin: EdgeInsets.only(left:20.0),
+                margin: EdgeInsets.only(left: 20.0),
                 padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   border: Border.all(),
@@ -39,19 +39,56 @@ class _ProductDetailState extends State<ProductDetail> {
             ),
             Expanded(
               child: Container(
-                padding: EdgeInsets.only(top:20.0,left:20.0),
-              margin: EdgeInsets.only(top: 20.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
+                padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                margin: EdgeInsets.only(top: 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Headphone",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                        ),
+                        Text(
+                          "\$300",
+                          style: TextStyle(
+                            color: Color(0xFFfd6f3e),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20), // Space between the Row and the "Details" text
+                    Text(
+                      "Details",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    Text("Branded headphone with 1 year waranty vdfferwffd fewdfwe ewaasda asdadvd fewfscd refvfdscdc refsdcds refsdwsdc refscbyrg uytgvdfve"),
+                    SizedBox(height: 90.0,),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                      decoration: BoxDecoration(color: Color(0xFFfd6f3e),borderRadius: BorderRadius.circular(10)),
+                      width: MediaQuery.of(context).size.width,
+                      child: Center(child: Text("Buy now",style: TextStyle(color:Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold),),),
+
+                    )
+                  ],
                 ),
               ),
-              width: MediaQuery.of(context).size.width,
-              child: Text("Headphone",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 30)),
             ),
-            )
           ],
         ),
       ),
